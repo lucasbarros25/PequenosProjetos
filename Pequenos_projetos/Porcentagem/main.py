39ff8292fe5ab51862b83174ea4967a41db1ae98
Lucas Outor Barros <lucasoutor@gmail.com>
2020-10-15 13:55:39 -0300

    Pequenos projetos de python

---
+++ Pequenos_projetos/Porcentagem/main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def porcentagem(a,b):
+    a = a * b
+    return a
+
+num =0
+porc = 0
+print("Qual a porcentagem que será usada?")
+porc = float(input()) / 100
+print("Qual o número  que necessita saber a porcentagem?")
+num = float(input())
+print(porcentagem(num,porc))
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
